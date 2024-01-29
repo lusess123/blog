a87d86238b41746e8413f0520c585d913ef0adf6
lusess123 <zyk2003xxx@163.com>
2024-01-29 19:21:27 +0800

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,56 @@
-import Image from 'next/image'
-// 提供插入md文档模板标签
-import ReactMarkdown from 'react-markdown';
-// 支持gfm语法 简单理解就是平时书写md文档的语法
-import remarkGfm from 'remark-gfm';
-// md文档所需要的样式，例如表格的线条等等
-import 'github-markdown-css';
-import rehypeRaw from 'rehype-raw'
-import fs from 'fs'
-import { join } from 'path'
+import React from 'react'
+import { getList, formatDateString } from '../util/file-util'
+import './page.scss'
+import Link from 'next/link'
 
-export default  function Home(props: any) {
-  console.log('props:', props)
-  let xd 
-  if(!(props?.params?.x)) {
-  const mdPath  = join(process.cwd(), 'blogs/read-daily/2024年01月.md')
-  console.log('mdPath render:', mdPath)
-  const md = fs.readFileSync(mdPath, 'utf8')
-  // console.log('md:', md)
-  xd = md.replaceAll('https://cdn.nlark.com', '/api?url=https://cdn.nlark.com') 
-  } else {
-    xd = props?.params?.x
+function convertDate(input: string) {
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  const year = input.substring(0, 4);
+  const monthIndex = parseInt(input.substring(4, 6), 10) - 1; // 月份索引（从0开始）
+
+  if (monthIndex < 0 || monthIndex > 11) {
+    return "Invalid month"; // 如果月份无效
   }
-  return (
-      <div>
-      <ReactMarkdown  className="markdown-body"
-      rehypePlugins={[rehypeRaw]}
-      remarkPlugins={[[remarkGfm,{ singleTilde: false }]]}>
-        {xd}
-        </ReactMarkdown>
-      </div>
-  )
-}
-export function generateStaticParams() {
-  console.log('-------generateStaticParams---------')
-  return []
+
+  const month = months[monthIndex];
+  return `${month} ${year}`;
 }
 
-// export function generateStaticParams() {
-//   const mdPath  = join(process.cwd(), 'blogs/read-daily/2023年12月.md')
-//   console.log('xmdPath:', mdPath)
-//   const md = fs.readFileSync(mdPath, 'utf8')
-//   console.log('x:', md)
-//   return [
-//      {
-//       x: md
-//      }
-//   ]
-// }
+// const result = convertDate("201602");
+// console.log(result); // 输出: "Feb 2016"
+
+
+export default async function Home() {
+  const list = await getList()
+  return (
+    <div>
+       <div className='text-center mt-9   text-blue-500 text-3xl font-bold '>每日阅读</div>
+
+    <section className="info-list gray">
+     
+      <ul className="news-list">
+        {
+          list.sort((a, b) => parseInt(b.day) - parseInt(a.day)).map((item) => {
+            return <li key={item.day} className="determine-hover enable-hover" data-type="article">
+              <a className="history-link" title="Article One Headline" href="#">
+                <div className="canvas-background">
+                  <div className="stalker-wrap"></div>
+                </div>
+                <div className="gradient-hover gradient-1"></div>
+                <div className="constrain">
+                  <span className="date">{convertDate(item.day)}</span>
+                 
+                  <p> <Link href={`/${item.day}`}>{formatDateString(item.day).replace(".md", "")}</Link></p>
+                </div>
+                <div className="gradient-hover gradient-2"></div>
+              </a>
+            </li>
+          })
+        }
+      </ul>
+    </section>
+    </div>
+  )
+
+}
